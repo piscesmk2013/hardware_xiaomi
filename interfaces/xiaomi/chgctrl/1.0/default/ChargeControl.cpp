piscesmk2013/hardware_oneplus

Add charge control HAL

interfaces/oneplus/chgctrl/1.0/default/ChargeControl.cpp
@@ -1,0 +1,100 @@
+/*
+ * Copyright (C) 2022 Project Kaleidoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "charge-control-hal"
+
+#include "ChargeControl.h"
+#include <android-base/logging.h>
+#include <fcntl.h>
+
+/*
+ * If the default node is not compatible, bake this HAL into your
+ * device tree and change the following macros.
+ */
+#define CHARGE_CONTROL_NODE "/sys/class/power_supply/battery/charging_enabled"
+#define CHARGE_ENABLED '1'
+#define CHARGE_DISABLED '0'
+
+namespace vendor::oneplus::chgctrl::implementation {
+
+static int readNode(char& ret) {
+    int fd;
+    int err = 0;
+
+    fd = open(CHARGE_CONTROL_NODE, O_RDONLY);
+    if (fd < 0)
+        return 1;
+
+    err = read(fd, &ret, sizeof(char));
+    close(fd);
+
+    if (err < 0)
+        return 1;
+
+    return 0;
+}
+
+static int writeNode(char value) {
+    int fd;
+    int err = 0;
+
+    fd = open(CHARGE_CONTROL_NODE, O_WRONLY);
+    if (fd < 0)
+        return 1;
+
+    err = write(fd, &value, sizeof(char));
+    close(fd);
+
+    if (err < 0)
+        return 1;
+
+    return 0;
+}
+
+// Methods from ::vendor::oneplus::chgctrl::V1_0::IChargeControl follow.
+Return<bool> ChargeControl::getChargeEnabled() {
+    char status = 0;
+
+    if (readNode(status))
+        LOG(ERROR) << "Unable to get status";
+
+    if (status == CHARGE_DISABLED)
+        return false;
+
+    return true;
+}
+
+// Return true on failure
+Return<bool> ChargeControl::setChargeEnabled(bool enabled) {
+    char value;
+
+    if (enabled)
+        value = CHARGE_ENABLED;
+    else
+        value = CHARGE_DISABLED;
+
+    return writeNode(value);
+}
+
+
+// Methods from ::android::hidl::base::V1_0::IBase follow.
+
+//IChargeControl* HIDL_FETCH_IChargeControl(const char* /* name */) {
+    //return new ChargeControl();
+//}
+//
+}  // namespace vendor::oneplus::chgctrl::implementation
+

interfaces/oneplus/chgctrl/1.0/default/ChargeControl.h
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2022 Project Kaleidoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <vendor/oneplus/chgctrl/1.0/IChargeControl.h>
+#include <hidl/MQDescriptor.h>
+#include <hidl/Status.h>
+
+namespace vendor::oneplus::chgctrl::implementation {
+
+using ::android::hardware::hidl_array;
+using ::android::hardware::hidl_memory;
+using ::android::hardware::hidl_string;
+using ::android::hardware::hidl_vec;
+using ::android::hardware::Return;
+using ::android::hardware::Void;
+using ::android::sp;
+
+struct ChargeControl : public V1_0::IChargeControl {
+    // Methods from ::vendor::charge::chgctrl::V1_0::IChargeControl follow.
+    Return<bool> getChargeEnabled() override;
+    Return<bool> setChargeEnabled(bool enabled) override;
+
+    // Methods from ::android::hidl::base::V1_0::IBase follow.
+
+};
+
+// FIXME: most likely delete, this is only for passthrough implementations
+// extern "C" IChargeControl* HIDL_FETCH_IChargeControl(const char* name);
+
+}  // namespace vendor::oneplus::chgctrl::implementation
+

interfaces/oneplus/chgctrl/1.0/default/service.cpp
@@ -1,0 +1,45 @@
+/*
+ * Copyright (C) 2022 Project Kaleidoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "charge-control-hal"
+
+#include <android-base/logging.h>
+#include <android-base/properties.h>
+#include <binder/ProcessState.h>
+#include <hidl/HidlTransportSupport.h>
+#include "ChargeControl.h"
+
+using vendor::oneplus::chgctrl::V1_0::IChargeControl;
+using vendor::oneplus::chgctrl::implementation::ChargeControl;
+
+int main() {
+    android::sp<IChargeControl> service = new ChargeControl();
+
+    android::hardware::configureRpcThreadpool(1, true /*callerWillJoin*/);
+
+    if (service->registerAsService() != android::OK) {
+        LOG(ERROR) << "Cannot register chgctrl HAL service.";
+        return 1;
+    }
+
+    LOG(INFO) << "chgctrl HAL service ready.";
+
+    android::hardware::joinRpcThreadpool();
+
+    LOG(ERROR) << "chgctrl HAL service failed to join thread pool.";
+    return 1;
+}
+

piscesmk2013/hardware_xiaomi

interfaces/xiaomi/chgctrl/1.0/default/ChargeControl.cpp
@@ -1,0 +1,100 @@
+/*
+ * Copyright (C) 2022 Project Kaleidoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "charge-control-hal"
+
+#include "ChargeControl.h"
+#include <android-base/logging.h>
+#include <fcntl.h>
+
+/*
+ * If the default node is not compatible, bake this HAL into your
+ * device tree and change the following macros.
+ */
+#define CHARGE_CONTROL_NODE "/sys/class/power_supply/battery/charging_enabled"
+#define CHARGE_ENABLED '1'
+#define CHARGE_DISABLED '0'
+
+namespace vendor::xiaomi::chgctrl::implementation {
+
+static int readNode(char& ret) {
+    int fd;
+    int err = 0;
+
+    fd = open(CHARGE_CONTROL_NODE, O_RDONLY);
+    if (fd < 0)
+        return 1;
+
+    err = read(fd, &ret, sizeof(char));
+    close(fd);
+
+    if (err < 0)
+        return 1;
+
+    return 0;
+}
+
+static int writeNode(char value) {
+    int fd;
+    int err = 0;
+
+    fd = open(CHARGE_CONTROL_NODE, O_WRONLY);
+    if (fd < 0)
+        return 1;
+
+    err = write(fd, &value, sizeof(char));
+    close(fd);
+
+    if (err < 0)
+        return 1;
+
+    return 0;
+}
+
+// Methods from ::vendor::xiaomi::chgctrl::V1_0::IChargeControl follow.
+Return<bool> ChargeControl::getChargeEnabled() {
+    char status = 0;
+
+    if (readNode(status))
+        LOG(ERROR) << "Unable to get status";
+
+    if (status == CHARGE_DISABLED)
+        return false;
+
+    return true;
+}
+
+// Return true on failure
+Return<bool> ChargeControl::setChargeEnabled(bool enabled) {
+    char value;
+
+    if (enabled)
+        value = CHARGE_ENABLED;
+    else
+        value = CHARGE_DISABLED;
+
+    return writeNode(value);
+}
+
+
+// Methods from ::android::hidl::base::V1_0::IBase follow.
+
+//IChargeControl* HIDL_FETCH_IChargeControl(const char* /* name */) {
+    //return new ChargeControl();
+//}
+//
+}  // namespace vendor::xiaomi::chgctrl::implementation
+

interfaces/xiaomi/chgctrl/1.0/default/ChargeControl.h
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2022 Project Kaleidoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <vendor/xiaomi/chgctrl/1.0/IChargeControl.h>
+#include <hidl/MQDescriptor.h>
+#include <hidl/Status.h>
+
+namespace vendor::xiaomi::chgctrl::implementation {
+
+using ::android::hardware::hidl_array;
+using ::android::hardware::hidl_memory;
+using ::android::hardware::hidl_string;
+using ::android::hardware::hidl_vec;
+using ::android::hardware::Return;
+using ::android::hardware::Void;
+using ::android::sp;
+
+struct ChargeControl : public V1_0::IChargeControl {
+    // Methods from ::vendor::charge::chgctrl::V1_0::IChargeControl follow.
+    Return<bool> getChargeEnabled() override;
+    Return<bool> setChargeEnabled(bool enabled) override;
+
+    // Methods from ::android::hidl::base::V1_0::IBase follow.
+
+};
+
+// FIXME: most likely delete, this is only for passthrough implementations
+// extern "C" IChargeControl* HIDL_FETCH_IChargeControl(const char* name);
+
+}  // namespace vendor::xiaomi::chgctrl::implementation
+

interfaces/xiaomi/chgctrl/1.0/default/service.cpp
@@ -1,0 +1,45 @@
+/*
+ * Copyright (C) 2022 Project Kaleidoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "charge-control-hal"
+
+#include <android-base/logging.h>
+#include <android-base/properties.h>
+#include <binder/ProcessState.h>
+#include <hidl/HidlTransportSupport.h>
+#include "ChargeControl.h"
+
+using vendor::xiaomi::chgctrl::V1_0::IChargeControl;
+using vendor::xiaomi::chgctrl::implementation::ChargeControl;
+
+int main() {
+    android::sp<IChargeControl> service = new ChargeControl();
+
+    android::hardware::configureRpcThreadpool(1, true /*callerWillJoin*/);
+
+    if (service->registerAsService() != android::OK) {
+        LOG(ERROR) << "Cannot register chgctrl HAL service.";
+        return 1;
+    }
+
+    LOG(INFO) << "chgctrl HAL service ready.";
+
+    android::hardware::joinRpcThreadpool();
+
+    LOG(ERROR) << "chgctrl HAL service failed to join thread pool.";
+    return 1;
+}
+